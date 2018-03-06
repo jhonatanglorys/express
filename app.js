6663df70c830c1a7ff4f579a44c1dfdfaaede8bb
jhonatanglorys <jhonatanglorys@gmail.com>
2018-03-06 16:28:21 -0500

    get method

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/persona', function(req, res) {
-    var user_id = req.param('id'); 
-    if (req.param('id')== '4'){
-        res.send(cualquiera.nombre);
-    } else {
-        res.send('No existe este usuario');
+app.get('/persona/:id', function(req, res) {
+    var user_id = req.param('id');
+    switch(user_id){
+        case 0:
+        res.send(cualquiera.nombre)
+        break;
+        case 1:
+        res.send(cualquiera.nombre)
+        break;
+        default:
+        res.send('Este usuario no existe');
     }
-    
   });
 
 app.listen(3000, function(req,res){
     console.log('Escuchando en el puerto 3000');
-})
+});
+var identificacion = 0;
 var persona = {
-      id : '4',
+      id : identificacion,
       nombre : null,
-      apellido:null,
-      edad:null,
+      apellido:'Perez',
+      edad:28,
   }
-  
+  identificacion++;
   var cualquiera = persona;
   cualquiera.nombre = 'Martin';
   var profesor = persona;
+  profesor.nombre = 'Andres';
   profesor.universidad = 'udea';
+  identificacion++;
   var estudiante = persona;
+  estudiante.nombre
   estudiante.carrera = 'derecho';
+  identificacion++;
